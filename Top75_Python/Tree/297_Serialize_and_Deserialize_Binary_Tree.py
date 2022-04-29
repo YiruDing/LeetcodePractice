19be33bae04289346feaf1fe43a44ca11b61d35c
Yi-Ru Ding <dingyr0925@gmail.com>
2022-04-29 15:17:43 -0400

    0429_part1

---
+++ Top75_Python/Tree/297_Serialize_and_Deserialize_Binary_Tree.py
@@ -0,0 +1,36 @@
+# Use preOrder....How to determine whtere the left tree stop?
+# if left == null,than go to the right till it's null
+# Then go to the right of the root...
+
+
+class Codec:
+
+    def serialize(self, root):
+        result = []
+
+        def dfs(node):
+            if not node:
+                result.append("N")
+                return
+            result.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ",".join(result)
+
+    def deserialize(self, data):
+        vals = data.split(",")
+        self.index = 0
+
+        def dfs():
+            if vals[self.index] == "N":
+                self.index += 1
+                return None
+            node = TreeNode(int(vals[self.index]))
+            self.index += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+
+        return dfs()
