6b6de15c5845938df2d827124b7dffe3974c982e
Yi-Ru Ding <dingyr0925@gmail.com>
2022-07-11 22:16:14 -0400

    July11

---
+++ Neetcode150/Binary Search/153_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def findMin(self, nums: list[int]) -> int:
+        result = nums[0]
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            if nums[l] < nums[r]:
+                result = min(result, nums[l])
+                break
+
+            middle = (l + r) // 2
+            result = min(result, nums[middle])
+            if nums[middle] >= nums[l]:
+                l = middle + 1
+            else:
+                r = middle - 1
+
+        return result
---
+++ Neetcode150/Binary Search/33_Search_in_Rotated_Sorted_Array.py
@@ -0,0 +1,37 @@
+class Solution:
+
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+# 我未成功的嘗試
+#             if nums[mid]>=target and target > nums[right] :
+# #                 左半邊，往右
+#                 right=mid-1
+#             elif nums[mid]<target and target < nums[left] :
+# #                 右半邊，往左
+#                 left=mid+1
+#             elif nums[mid]>target and target < nums[right] :
+#                 left=mid+1
+# #               右半邊，往右
+#             elif nums[mid]<target and target < nums[right] :
+#                 right=mid-1
+                
+
+            if nums[mid] >= nums[left]:
+                if target > nums[mid] or target < nums[left]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if target < nums[mid] or target > nums[right]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+        return -1
