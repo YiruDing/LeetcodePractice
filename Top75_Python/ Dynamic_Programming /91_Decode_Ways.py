3dada65d2252b83b65544ec80fc4b3f22b17af8e
Yi-Ru Ding <dingyr0925@gmail.com>
2022-08-26 14:08:06 -0400

    Aug 26 part2

--- Top75_Python/ Dynamic_Programming /91_Decode_Ways.py
+++ Top75_Python/ Dynamic_Programming /91_Decode_Ways.py
@@ -1,3 +1,27 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        # Memoization
+        dp = {len(s): 1}
+
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            if s[i] == "0":
+                return 0
+
+            res = dfs(i + 1)
+            # 跟DP不同處，在於有res儲值
+            if i + 1 < len(s) and (
+                s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"
+            ):
+                res += dfs(i + 2)
+            # 最後update dp[i]的職為res
+            dp[i] = res
+            return res
+
+        return dfs(0)
+
+# Dynamic Programming
 # Similar to House Robber
 class Solution:
     def numDecodings(self, s: str) -> int:
