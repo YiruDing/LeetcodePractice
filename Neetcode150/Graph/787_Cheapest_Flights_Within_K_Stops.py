237276893c850d74de175bdb1d1a93ec82231616
Yi-Ru Ding <dingyr0925@gmail.com>
2022-08-29 08:24:28 -0400

    Aug 29

--- Neetcode150/Graph/787_Cheapest_Flights_Within_K_Stops.py
+++ Neetcode150/Graph/787_Cheapest_Flights_Within_K_Stops.py
@@ -17,3 +17,26 @@ class Solution:
             prices = tmpPrices
             # June9：pay attention on the indentation
         return -1 if prices[dst] == float('inf') else prices[dst]
+    
+# another possible solution(to be fixed...)
+class Solution:
+
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int,
+                          dst: int, k: int) -> int:
+            for u,v,p in flights:
+            edges[u].append((v,p))
+        
+        minHeap=[(0,src)]
+        visit=set()
+        p=0
+        while minHeap:
+            w1,n1=heapq.heappop(minHeap)
+            if n1 in visit:
+                continue
+            visit.add(n1)
+            p=max(p,w1)
+            
+            for n2,w2 in edges[n1]:
+                if n2 not in visit:
+                    heapq.heappush(minHeap,(w1+w2,n2))
+        return p 
