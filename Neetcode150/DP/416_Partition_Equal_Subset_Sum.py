064b37353d5f758b79fb699e1a1dbe194893d532
Yi-Ru Ding <dingyr0925@gmail.com>
2022-09-11 22:40:26 -0400

    Sep 11

--- Neetcode150/DP/416_Partition_Equal_Subset_Sum.py
+++ Neetcode150/DP/416_Partition_Equal_Subset_Sum.py
@@ -1,43 +1,43 @@
 class Solution:
 
-    # def canPartition(self, A):
-    #     t = sum(A) / 2
-    #     P = set([A[0]])
-    #     # 因為只要有一個值即可，這樣能省時間
-    #     for x in A[1:]:
-    #         for y in list(P):
-    #             # 記得嗎？set沒有按照順序喔，檢索應該比較麻煩...
-    #             P.add(x + y)
-    #     return t in P
+    def canPartition(self, nums):
+        t = sum(nums) / 2
+        P = set([nums[0]])
+        # 因為只要有一個值即可，這樣能省時間
+        for x in nums[1:]:
+            for y in list(P):
+                # set儲存沒有按照順序喔，iterate應該比較麻煩...
+                P.add(x + y)
+        print('P :',P)
+        # P : {1, 6, 11, 12, 17, 22}
+        return t in P
+    #   !!! 怎麼能據此確定nums的值可以對半分呢？
+
 
 # My solution(to be fixed):
-# 9/10 我想，答案錯誤應該是是因為必須考慮不同的排列組合，不能線性地疊加...
-# 在解題過程中，我print出問題，重新命名nums.sort()也出問題，可以幫我看一下嗎？
-    def canPartition(self, nums: list[int]) -> bool:
-# 在terminal用"Python3 416_Partition_Equal_Subset_Sum.py",得到如下回應：
-# line 14, in Solution
-# TypeError: 'type' object is not subscriptable
-# StackOverFlow
-        nums.sort()
-        res1, res2 = 0, 0
-        target = sum(nums) / 2
+# 9/10 答案錯誤是因為必須考慮不同的排列組合，不能線性地疊加...
+# 在解題過程中，重新命名nums.sort()出了問題，可以幫我看一下嗎？
 
-        l, r = 0, len(nums) - 1
-        while l < r:
-            if res1 < target:
-                res1 += nums[l]
-            elif res2 < target:
-                res2 += nums[r]
-            print('res1:', res1)
-            print('res2:', res2)
-            if res1 == target:
-                return sum(nums[l + 1:]) == target
-            elif res2 == target:
-                return sum(nums[:r]) == target
-            l += 1
-            r -= 1
-        return False
+    # def canPartition(self, nums):
+    #     nums.sort()
+    #     res1, res2 = 0, 0
+    #     target = sum(nums) / 2
 
+    #     l, r = 0, len(nums) - 1
+    #     while l < r:
+    #         if res1 < target:
+    #             res1 += nums[l]
+    #         elif res2 < target:
+    #             res2 += nums[r]
+    #         print('res1:', res1)
+    #         print('res2:', res2)
+    #         if res1 == target:
+    #             return sum(nums[l + 1:]) == target
+    #         elif res2 == target:
+    #             return sum(nums[:r]) == target
+    #         l += 1
+    #         r -= 1
+    #     return False
 
-tmp = Solution
+tmp = Solution()
 tmp.canPartition([1, 5, 11, 5])
--- Top75_Python/Dynamic_Programming/62_Unique_Paths.py
+++ Top75_Python/Dynamic_Programming/62_Unique_Paths.py
@@ -1,11 +1,37 @@
+# bottom up 為什麼省時很多？(50%)
+
 class Solution:
 
     def uniquePaths(self, m: int, n: int) -> int:
         row = [1] * n
-
+        # 為了之後能update和return，有必要在外面宣告一個值
         for i in range(m - 1):
             newRow = [1] * n
             for j in range(n - 2, -1, -1):
                 newRow[j] = newRow[j + 1] + row[j]
+                # newRow[j + 1]為右方的值 row[j]為下方的值
             row = newRow
         return row[0]
+
+
+# 順序
+# 1
+class Solution:
+    # @return an integer
+    def uniquePaths(self, m, n):
+        aux = [[1 for x in range(n)] for x in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                aux[i][j] = aux[i][j - 1] + aux[i - 1][j]
+        return aux[-1][-1]
+
+
+# 2 Ｔime complexity差好多耶(8%)！為什麼呢？
+class Solution:
+
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] = dp[j - 1] + dp[j]
+        return dp[-1] if m and n else 0
