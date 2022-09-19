8c4decdcc74d69fbdb546db5c30a3ea29fca21c1
Yi-Ru Ding <dingyr0925@gmail.com>
2022-09-19 08:59:04 -0400

    Sep 19

--- Neetcode150/Linked_List/92_Reverse_Linked_List_II.py
+++ Neetcode150/Linked_List/92_Reverse_Linked_List_II.py
@@ -38,3 +38,36 @@ class Solution:
 
 ans = Solution()
 ans.reverseBetween([1, 2, 3, 4, 5], 1, 4)
+
+
+# Neetcode
+# Break the pointer,re-connect to prev
+class Solution:
+
+    def reverseBetween(self, head: Optional[ListNode], left: int,
+                       right: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+
+        # 1)Reach node at position "left"
+        leftPrev, cur = dummy, head
+        for i in range(left - 1):
+            leftPrev, cur = cur, cur.next
+
+        # Now cur="left",leftPrev="node before left"
+        # 2)reverse from left to right
+        prev = None
+        for i in range(right - left + 1):
+            tmpNext = cur.next
+            cur.next = prev
+            prev = cur
+            cur = tmpNext
+
+# 14:34
+# 3) Updating the pointer(Connect leftPrev and prev)
+        leftPrev.next.next = cur
+        leftPrev.next = prev
+        return dummy.next
+
+
+#JS 解法：
+# https://replit.com/@ZhangMYihua/M-N-reversals#index.js
