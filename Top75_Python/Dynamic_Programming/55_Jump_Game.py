91a2117535ba1d50e2372df329122fc9e3c63b52
Yi-Ru Ding <dingyr0925@gmail.com>
2022-09-30 22:20:44 -0400

    Sep 30 part2

--- Top75_Python/Dynamic_Programming/55_Jump_Game.py
+++ Top75_Python/Dynamic_Programming/55_Jump_Game.py
@@ -1,10 +1,27 @@
 class Solution:
+
     def canJump(self, nums: List[int]) -> bool:
-        goal=len(nums)-1
-        
-        for i in range(len(nums)-1,-1,-1):
-            if i+nums[i]>=goal:
-                goal=i
-        
-        return True if goal==0 else False
-        
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+
+        return True if goal == 0 else False
+
+
+# My solution...
+# To be fixed...
+class Solution:
+
+    def canJump(self, nums: List[int]) -> bool:
+        res = [[False] * len(nums)]
+        res[-1] = True
+        for i in range(len(nums) - 1, -1, -1):
+            if i - nums[i] > 0:
+                n = nums[i]
+                while n:
+                    res[i - n] = True
+                    # list assignment index out of range
+                    n -= 1
+        return res[0]
