87232f4fe967d0915240ba58de2132ed096056e8
Yi-Ru Ding <dingyr0925@gmail.com>
2022-10-02 23:36:36 -0400

    Oct2 part2

--- Neetcode150/Math&Geometry/2013_Detect_Squares.py
+++ Neetcode150/Math&Geometry/2013_Detect_Squares.py
@@ -1,18 +1,23 @@
 class DetectSquares:
 
     def __init__(self):
-        self.ptsCount=defaultdict(int)
-        self.pts=[]
+        self.ptsCount = defaultdict(int)
+        self.pts = []
 
     def add(self, point: List[int]) -> None:
-        self.ptsCount[tuple(point)]+=1
+        self.ptsCount[tuple(point)] += 1
         self.pts.append(point)
 
     def count(self, point: List[int]) -> int:
-        res=0
-        px,py=point
-        for x,y in self.pts:
-            if (abs(py-y)!= abs(px-x))or x==px or y ==py:
+        res = 0
+        px, py = point
+        for x, y in self.pts:
+            # !!Look for the diagonal point對角線的點
+            if (abs(py - y) != abs(px - x)) or x == px or y == py:
+                # It x,y could't form the square or it's the same as ponit
                 continue
-            res+=self.ptsCount[(x,py)]*self.ptsCount[(px,y)]
+            res += self.ptsCount[(x, py)] * self.ptsCount[(px, y)]
+            # 5:44
+            # !!These are the other 2 points that could form the square!!
+            # Consider that there might be duplicate...
         return res
