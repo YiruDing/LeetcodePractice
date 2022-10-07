a3d1e4f1d1ebcbbc1234d4de0c6160b5a085bea7
Yi-Ru Ding <dingyr0925@gmail.com>
2022-10-06 21:55:43 -0400

    Oct6

--- Neetcode150/Math&Geometry/202_Happy_Number.py
+++ Neetcode150/Math&Geometry/202_Happy_Number.py
@@ -1,6 +1,15 @@
-from unicodedata import digit
+#Use set to make sure it would not be a endless loop
 
+# 比較帥的解法
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n not in seen:
+            seen.add(n)
+            n = sum([int(x) **2 for x in str(n)])
+        return n == 1
 
+# Neetcode 詳解
 class Solution:
     def isHappy(self, n: int) -> bool:
         visit=set()
@@ -23,4 +32,4 @@ class Solution:
                 n=n//10
                 
             return output
-            
+        
--- Neetcode150/Math&Geometry/66_Plus_One.py
+++ Neetcode150/Math&Geometry/66_Plus_One.py
@@ -1,17 +1,26 @@
 class Solution:
+
     def plusOne(self, digits: List[int]) -> List[int]:
-        digits=digits[::-1]
-        one,i=1,0
-        
+        digits = digits[::-1]
+        one, i = 1, 0
+
         while one:
             if i < len(digits):
-                if digits[i]==9:
-                    digits[i]=0
+                if digits[i] == 9:
+                    digits[i] = 0
                 else:
-                    digits[i]+=1
-                    one=0
+                    digits[i] += 1
+                    one = 0
             else:
                 digits.append(1)
-                one=0
-            i+=1
-        return digits[::-1]    
+                one = 0
+            i += 1
+            print("digits:", digits)
+        return digits[::-1]
+
+
+# [1,9,9,9]
+# digits: [0, 9, 9, 1]
+# digits: [0, 0, 9, 1]
+# digits: [0, 0, 0, 1]
+# digits: [0, 0, 0, 2]
