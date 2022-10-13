fc5a42b66fc1027cf483476daa5f97e5bb401d13
Yi-Ru Ding <dingyr0925@gmail.com>
2022-10-13 11:10:42 -0400

    Oct 13 part2

--- Top75_Python/Liked_List/23_Merge_k_Sorted_Lists.py
+++ Top75_Python/Liked_List/23_Merge_k_Sorted_Lists.py
@@ -33,6 +33,8 @@ class Solution:
                 tail.next = L2
                 L2 = L2.next
             tail = tail.next
+            # 這裡的tail.next就是從上面取得的值
+            #！！tail 也要往下走，才不會只在原地更改值
 
         if L1:
             tail.next = L1
@@ -42,3 +44,40 @@ class Solution:
         return result.next
 
     # Not tail.next!!
+
+
+# 下解...
+# 搭配heap來找出最小值，這個我懂，但是用counter是為什麼呢？
+import heapq
+
+class Solution:
+
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        h, counter = [], 0
+        for ls in lists:
+            if not ls:
+                continue
+            heapq.heappush(h, (ls.val, counter, ls))
+            # heapq.heappush(heap, item)
+            # Push the value item onto the heap, maintaining the heap invariant.
+            # 為何item長這個樣子(current.next.val, counter, current.next)？？
+            print("h:", h)
+            # 一次append一組Linked list??
+            # h: [(1, 0, ListNode{val: 1, next: ListNode{val: 4, next: ListNode{val: 5, next: None}}})]
+            # h: [(1, 0, ListNode{val: 1, next: ListNode{val: 4, next: ListNode{val: 5, next: None}}}), (1, 1, ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}})]
+            # h: [(1, 0, ListNode{val: 1, next: ListNode{val: 4, next: ListNode{val: 5, next: None}}}), (1, 1, ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}), (2, 2, ListNode{val: 2, next: ListNode{val: 6, next: None}})]
+
+            counter += 1
+        head = None
+        while h:
+            _, _, current = heapq.heappop(h)
+            # (ls.val, counter, ls)
+            head = head or current
+            # current是linked list唷
+            if current.next:
+                heapq.heappush(h, (current.next.val, counter, current.next))
+                counter += 1
+            if h:
+                current.next = h[0][2]
+                # current.next的值
+        return head
