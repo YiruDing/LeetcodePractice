3e66e3437b5919bde8f5edc1a211add64823214b
Yi-Ru Ding <dingyr0925@gmail.com>
2022-10-20 22:42:27 -0400

    Oct 20

--- Neetcode150/Stack/84_Largest_Rectangle_in_Histogram.py
+++ Neetcode150/Stack/84_Largest_Rectangle_in_Histogram.py
@@ -1,3 +1,4 @@
+
 class Solution:
 
     def largestRectangleArea(self, heights: List[int]) -> int:
@@ -16,3 +17,10 @@ class Solution:
         for i, h in stack:
             res = max(res, h * (len(heights) - i))
         return res
+# [2,1,5,6,2,3]    
+# stack: [(0, 2)]
+# stack: [(0, 1)]
+# stack: [(0, 1), (2, 5)]
+# stack: [(0, 1), (2, 5), (3, 6)]
+# stack: [(0, 1), (2, 2)]
+# stack: [(0, 1), (2, 2), (5, 3)]
