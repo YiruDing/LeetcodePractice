68f1dcc066a37907f287af72fa4a97d283647246
Yi-Ru Ding <dingyr0925@gmail.com>
2022-11-11 22:55:19 -0500

    Nov 11

--- Neetcode305/Array/119_Pascal's_Triangle_II.py
+++ Neetcode305/Array/119_Pascal's_Triangle_II.py
@@ -4,6 +4,7 @@ class Solution:
         pascal = [1] * (rowIndex + 1)
         for i in range(2, rowIndex + 1):
             for j in range(i - 1, 0, -1):
+                # i-1 !!
                 pascal[j] += pascal[j - 1]
         return pascal
 
--- Neetcode305/Array/27_Remove_Element.py
+++ Neetcode305/Array/27_Remove_Element.py
@@ -15,7 +15,8 @@ class Solution:
         start, end = 0, len(nums) - 1
         while start <= end:
             if nums[start] == val:
-                nums[start], nums[end], end = nums[end], nums[start], end - 1
+                nums[start] = nums[end]
+                end -= 1
             else:
                 start += 1
         return start
