5ef5efe6b9f50d2c5c5c224080240b524d575b6e
Yi-Ru Ding <dingyr0925@gmail.com>
2022-11-23 20:18:47 -0500

    Oct 23

--- Neetcode305/Linked_List/147_Insertion_Sort_List.py
+++ Neetcode305/Linked_List/147_Insertion_Sort_List.py
@@ -3,6 +3,7 @@ class Solution:
     def insertionSortList(self,
                           head: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode(0, head)
+        # !!ListNode(0, head)
         prev, cur = head, head.next
 
         while cur:
@@ -12,13 +13,19 @@ class Solution:
                 continue
 
             tmp = dummy
+            # 逆向回去確認前值
             while cur.val > tmp.next.val:
+                # !!不是tmp.val
                 tmp = tmp.next
+                # 勿忘這兩行！
                 # 繼續往前走，找到插入點
             prev.next = cur.next
+            # prev跟下下一個接在一起了
             cur.next = tmp.next
             # assign tmp.next before reassign it
+            # cur指向上一個，也就是交換位置swap
             tmp.next = cur
+            # tmp指向cur，完成swap
             cur = prev.next
             #  prev.next 就是 cur.next
 
