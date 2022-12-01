b95917c48df627a94fac56d8a9249bc11049dd40
Yi-Ru Ding <dingyr0925@gmail.com>
2022-12-01 09:31:33 -0500

    Dec 1

--- Neetcode305/Heap/215_Kth_Largest_Element_in_an_Array.py
+++ Neetcode305/Heap/215_Kth_Largest_Element_in_an_Array.py
@@ -1,4 +1,6 @@
 # 8/6 哇！我解開了！
+# 11/29 minHeap of size K
+
 
 class Solution:
 
@@ -10,5 +12,46 @@ class Solution:
             heapq.heappop(nn)
             k -= 1
         return -nn[0]
-    
+
+
 # Neetcode
+class Solution:
+
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+
+        def __init__(self, k: int, nums: List[int]):
+            self.minHeap = nums
+            self.k = 
+            heapq.heapify(self.minHeap)
+            # 11/29 記得heapify!!
+            while len(self.minHeap) > k:
+                heapq.heappop(self.minHeap)
+
+        def add(self, val: int) -> int:
+            heapq.heappush(self.minHeap, val)
+            if len(self.minHeap) > self.k:
+                heapq.heappop(self.minHeap)
+            return self.minHeap[0]
+
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        # https://docs.python.org/3/library/heapq.html
+        # heapq.heappush(heap, item)
+        # Push the value item onto the heap, maintaining the heap invariant.
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
--- Neetcode305/Heap/973_K_Closest_Points_to_Origin.py
+++ Neetcode305/Heap/973_K_Closest_Points_to_Origin.py
@@ -5,14 +5,35 @@ class Solution:
         for x, y in points:
             dist = (x**2) + (y**2)
             minHeap.append([dist, x, y])
-            # Python will sortthe first value by default
+            # Python will sort the first value by default
 
         heapq.heapify(minHeap)
         result = []
         while k > 0:
             dist, x, y = heapq.heappop(minHeap)
             result.append([x, y])
-            # K Closest Points 
+            # K Closest Points
             k -= 1
 
         return result
+
+
+# Leetcode 另解(11/29 這個比較好！)
+# Maintain a heap with length K, with maximum distance element stored on top
+# Since python api is min-heap, we use negative distance here
+# When the heap reaches length K, if next distance is smaller than current largest distance, the top element could be updated
+class Solution:
+
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+
+        res = []
+
+        for i, j in points:
+            x = i * i + j * j
+
+            if len(res) < k:
+                heapq.heappush(res, (-x, [i, j]))
+            else:
+                heapq.heappushpop(res, (-x, [i, j]))
+
+        return [x[1] for x in res]
