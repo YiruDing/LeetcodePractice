012da344f58dfe5fe8418a18afb4e3b7d54612bf
Yi-Ru Ding <dingyr0925@gmail.com>
2023-02-04 11:13:35 -0500

    Feb 4

--- Neetcode305/Math&Geometry/48_Rotate_Image(Udemy中文亦有).py
+++ Neetcode305/Math&Geometry/48_Rotate_Image(Udemy中文亦有).py
@@ -51,3 +51,40 @@ class Solution:
 # topLeft:  2
 # topLeft:  3
 # topLeft:  6
+
+
+# 官方解答
+# Approach 1: Rotate Groups of Four Cells
+class Solution:
+
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix[0])
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                # 2/3 這兩行我不懂...可以請你說明一下嗎？
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
+
+
+# Approach 2: Reverse on Diagonal and then Reverse Left to Right
+class Solution:
+
+    def rotate(self, matrix: List[List[int]]) -> None:
+        self.transpose(matrix)
+        self.reflect(matrix)
+
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j -
+                                                            1], matrix[i][j]
