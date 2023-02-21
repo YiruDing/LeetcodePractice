a543e6bddde15aef68099f58eac31abacae09776
Yi-Ru Ding <dingyr0925@gmail.com>
2023-02-21 17:58:42 -0500

    Feb 21

--- Neetcode305/Array&Hashing/605_Can_Place _Flowers.py
+++ Neetcode305/Array&Hashing/605_Can_Place _Flowers.py
@@ -1,4 +1,4 @@
-# 1
+# Neetcode 1
 class Solution:
 
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
@@ -8,4 +8,21 @@ class Solution:
             if f[i - 1] == 0 and f[i] == 0 and f[i + 1] == 0:
                 f[i] = 1
                 n -= 1
+        return n <= 0
+    
+# Neetcode 2
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        
+        empty = 0 if flowerbed[0] else 1
+        for f in flowerbed:
+            if f:
+                # 2/21 此位置有花
+                n -= int((empty - 1)/2) # int division, round toward zero
+                # 2/21 ??
+                empty = 0
+            else:
+                empty += 1
+        n -= int(empty) // 2
         return n <= 0
